c36684068cb638fe3346d6b7ef6582dc99f8fecb
Artyom <room5525969@mail.ru>
2021-05-11 20:53:34 +0300

    push

--- src/utils/useSlidesAmount.js
+++ src/utils/useSlidesAmount.js
@@ -6,7 +6,6 @@ export function useSlidesAmount(slidesAmount) {
 	const [visibleSlides, setVisibleSlides] = React.useState(slidesAmount);
 
 	React.useEffect(() => {
-
 		function handleResize() {
 			let width = window.innerWidth;
 
@@ -15,7 +14,6 @@ export function useSlidesAmount(slidesAmount) {
 			} else {
 				setVisibleSlides(slidesAmount);
 			}
-
 		}
 
 		window.addEventListener('resize', throttle(handleResize, 200));
